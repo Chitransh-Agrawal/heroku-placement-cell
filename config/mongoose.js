7dbeef04b4344473a638c0acfc0619265bedc5d9
Chitransh Agrawal <chitranshagrawal0@gmail.com>
2022-09-30 16:23:53 +0530

    db updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(`mongodb+srv://chitransh-agrawal:lLHwm1Bi0dS0oGw2@cluster0.piqqzwo.mongodb.net/?retryWrites=true&w=majority`,
-{dbName:placement_cell});
+{dbName:'placement_cell'});
 // ("mongodb://localhost/placement_DB");
 const db = mongoose.connection;
 
